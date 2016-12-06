b840c5f1e66fadf4aa44ac47403e41f046af4ceb
Andrew Raim <andrew.raim@census.gov>
2016-12-06 17:41:34 -0500

    For debugging, load eigvals and eigvects from Matlab

--- matlab/save2csv.m
+++ matlab/save2csv.m
@@ -2,11 +2,16 @@
 [I,J] = find(H);
 csvwrite('H_sparse.txt', [I, J, nonzeros(H)]);
 
-% Sparse matrix format
 [I,J] = find(S1);
 csvwrite('S1_sparse.txt', [I, J, nonzeros(S1)]);
+
+[I,J] = find(HpinvVH);
+csvwrite('HpinvVH_sparse.txt', [I, J, nonzeros(HpinvVH)]);
+
+[I,J] = find(EigHinvVHp);
+csvwrite('EigHinvVHp_sparse.txt', [I, J, nonzeros(EigHinvVHp)]);
 
 csvwrite('Zagg.txt', Zagg);
 csvwrite('sigmavar.txt', sigmavar);
 csvwrite('Kinv.txt', Kinv);
-
+csvwrite('LamHpinvVH.txt', diag(LamHpinvVH));
