e80a03b1c6cb6d8e9984105e1f4114d7e2c42370
Andrew Raim <andrew.raim@gmail.com>
2017-01-16 18:52:29 -0500

    Working on basis function code

--- R/areal_bi2.R
+++ R/areal_bi2.R
@@ -1,8 +1,7 @@
-# TBD: Keep drawing until we get B draws
+# Keep drawing until we get n draws
 # Also, might want to consider just picking points on a grid
-function randomly_generate_Ds <- function(A, B)
+sample_Ds <- function(n, A)
 {
-	stopifnot(length(times) == 1)
 	stopifnot(class(A) == "SpatialPolygonsDataFrame")
 
 	lon.low <- A@bbox[1,1]
@@ -10,88 +9,90 @@ function randomly_generate_Ds <- function(A, B)
 	lat.low <- A@bbox[2,1]
 	lat.hi <- A@bbox[2,2]
 
-	u.lon <- runif(n, lon.low, lon.hi)
-	u.lat <- runif(n, lat.low, lat.hi)
-	u <- data.frame(Longitude = u.lon, Latitude = u.lat)
-	coordinates(u) <- ~ Longitude + Latitude
-	proj4string(u) <- proj4string(A)
+	uu <- matrix(NA, 0, 2)
+	n.cur <- 0
 
-	# Now reject the ones outside of the area
-	idx <- which(!is.na(over(u, A)$GEO_ID))
-	uu <- u@coords[idx,]
+	while (n.cur < n) {
+		u.lon <- runif(n, lon.low, lon.hi)
+		u.lat <- runif(n, lat.low, lat.hi)
+		u <- data.frame(Longitude = u.lon, Latitude = u.lat)
+		coordinates(u) <- ~ Longitude + Latitude
+		proj4string(u) <- proj4string(A)
+
+		# Only accept the ones inside the target area
+		idx <- which(!is.na(over(u, A)[,1]))
+		uu <- rbind(uu, u@coords[idx,])
+		n.cur <- nrow(uu)
+	}
 
 	# plot(A)
 	# points(u[-idx], col = "black")
 	# points(u[idx], col = "red")
-
-	return(uu)
+	return(uu[1:n,])
 }
 
-function ArealBi2 <- function(D, times, level1, level2, level3, B, srad, trad)
+ArealBi2 <- function(D, times, level1, level2 = matrix(NA, 0, 0),
+	level3 = matrix(NA, 0, 0), B, srad, trad, report.period = 1)
 {
 	r <- nrow(level1) + nrow(level2) + nrow(level3)
 	n <- length(D)
-	S <- matrix(0, n, r)
-	T <- max(length(times))
+	S <- Matrix(0, n, r)
+	T <- length(times)
 
-	# s1{t} and s2{t} are n x B matrices
+	# s.lon and s.lat are n x B matrices
+	# Note that we don't need to generate different draws over time for the same space
+	s.lon <- matrix(NA, n, B)
+	s.lat <- matrix(NA, n, B)
 
-	for (t in 1:T) {
-		for (j in 1:n) {
-			points2 <- randomly_generate_Ds(D[j], B)
-			s2{t}(j,:) <- points2(:,1)
-			s1{t}(j,:) <- points2(:,2)
+	for (i in 1:n) {
+		p <- sample_Ds(B, D[i])
+		s.lon[i,] <- p[,1]
+		s.lat[i,] <- p[,2]
+	}
+
+	for (b in 1:B) {
+		if (b %% report.period == 0) { logger("Iteration %d\n", b) }
+		# TBD: I switched the order of the coordinates here. Was that a mistake?
+		P <- cbind(s.lon[,b], s.lat[,b])
+		for (t in 1:T) {
+			Sq <- Create_S(P, times[t], level1, level2, level3, srad, trad)
+			S <- S + Sq
 		}
 	}
 
-	for (t in 1:T) {
-		for (i in 1:B) {
-			if (i %% 1 == 0) { logger(" Iteration %d", i) }
-			Sq <- Create_S([s2{t}(:,i), s1{t}(:,i)], times[t], level1, level2, level3, srad, trad)
-			S <- Sq + S
-		}
-	}
-
-	S <- S / (B*T)
+	S / (B * T)
 }
 
-
-function S <- Create_S(crd, times, level1, level2, level3, w, tw)
+Create_S <- function(crd, times, level1, level2, level3, w, tw)
 {
-	level{1} = level1
-	level{2} = level2
-	level{3} = level3
-
-	for i = 1:3
-		if isempty(level{i}) == 0
-			[B,IX] = sort(level{i},1)
-			level{i} = level{i}(IX(:,2),:)
-		else
-			level{i} = []
-		end
-	end
-
-	G = dist(level1)
-	rl = [w*quantile(G(G>0),0.05), w*quantile(pdist(level2),0.05), w*quantile(pdist(level3),0.05)]
-	S = matrix(0, nrow(crd), nrow(level1) + nrow(level2) + nrow(level3))
-
-	count = 0
-	for (i in 1:3) {
-		if (!isempty(level{i}) == 0) {
-			for (j in 1:nrow(crd)) {
-				count = count + 1
-				h = sqrt((level{i}(:,1) - crd(j,1))^2 + (level{i}(:,2) - crd(j,2))^2)
-				ht = sqrt((level{i}(:,3) - times)^2)
-				s = (1 - (h/rl(1,i))^2 - (ht/tw)^2)^2
-				s[h > rl[1,i]] = 0
-				if (i == 1) {
-					S(j,1:length(level{i})) = s
-				} else if (i == 2) {
-					S(j,(length(level{1})+1):(length(level{1})+length(level{2}))) = s
-				} else {
-					S(j,(length(level{1})+length(level{2})+1):(length(level{1})+length(level{2})+length(level{3}))) = s
-				}
-			}
-		}
+	if (nrow(level2) > 0 || nrow(level3) > 0) {
+		stop("Levels 2 and 3 not supported in this implementation")
 	}
+
+	# AMR: Do we need this?
+	# if isempty(level{i}) == 0
+	#	[B,IX] = sort(level1,1)
+	#	level1 = level1(IX(:,2),:)
+	# else
+	#	level1 = []
+	# end
+
+	G <- dist(level1)
+	rl <- w * quantile(G[G > 0],0.05)
+	S <- matrix(0, nrow(crd), nrow(level1))
+
+	if (nrow(level1) == 0) {
+		return(S)
+	}
+
+	for (j in 1:nrow(crd)) {
+		h <- sqrt((level1[,1] - crd[j,1])^2 + (level1[,2] - crd[j,2])^2)
+		ht <- sqrt((level1[,3] - times)^2)
+		s <- (1 - (h / rl)^2 - (ht / tw)^2)^2
+		# s[h > rl | ht > tw] <- 0
+		s[h > rl] <- 0
+		S[j,] <- s
+	}
+
+	return(S)
 }
