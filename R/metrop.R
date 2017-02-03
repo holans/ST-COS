1b01fe5e15eb13ce991d62dbefcda36ced0ecb36
Andrew Raim <andrew.raim@gmail.com>
2017-02-02 20:09:53 -0500

    Updates to metrop method

--- R/metrop.R
+++ R/metrop.R
@@ -25,9 +25,17 @@ metrop.stcos <- function(Z, S, sig2eps, C.inv, H, R,
 	sig2mu <- init$sig2mu
 	sig2xi <- init$sig2xi
 	sig2K <- init$sig2K
+
 	logger("Begin sampling\n")
 
-	Data <- list(y = y, X = cbind(H, S), sig2eps = sig2eps)
+
+	# A workaround, since some of the eigvals in C.inv are negative
+	# Perhaps because they were transferred from Matlab via CSV.
+	eigvals <- eigen(C.inv)$values
+	eigvals[eigvals < 0] <- -1*eigvals[eigvals < 0]
+	logdet.Cinv <- sum(log(eigvals))
+
+	Data <- list(y = Z, X = cbind(H, S), sig2eps = sig2eps, logdet.Cinv = logdet.Cinv)
 
 	# Log-posterior for [sig2mu, sig2xi, sig2K | Z]
 	logpost <- function(par, Data) {
@@ -35,31 +43,40 @@ metrop.stcos <- function(Z, S, sig2eps, C.inv, H, R,
 		sig2K <- exp(par[2])
 		sig2xi <- exp(par[3])
 
-		G <- Matrix(0, n_mu+r, n_mu+r)
-		G[cbind(1:n_mu, 1:n_mu)] <- sig2mu
-		G[1:r + n_mu, 1:r + n_mu] <- sig2K * solve(C.inv)
-		
+		# G <- Matrix(0, n_mu+r, n_mu+r)
+		# G[cbind(1:n_mu, 1:n_mu)] <- sig2mu
+		# G[1:r + n_mu, 1:r + n_mu] <- sig2K * solve(C.inv)
+
 		Ginv <- Matrix(0, n_mu+r, n_mu+r)
 		Ginv[cbind(1:n_mu, 1:n_mu)] <- 1/sig2mu
 		Ginv[1:r + n_mu, 1:r + n_mu] <- 1/sig2K * C.inv
 
-		Omega <- Diagonal(1/sig2eps + 1/sig2xi, n)
-		Gamma <- t(Data$X) %*% Omega %*% Data$X + Ginv
-		nu <- solve(Gamma, t(Data$X) %*% Omega %*% Data$y)
+		Omega <- 1/Data$sig2eps + 1/sig2xi
+		Gamma <- t(Data$X) %*% (Omega * Data$X) + Ginv
+		nu <- solve(Gamma, t(Data$X) %*% (Omega * Data$y))
 
 		# logdet.Omega <- sum(log(eigen(Omega)$values))
-		logdet.Omega <- sum(log(1/sig2eps + 1/sig2xi))
-		logdet.Gamma <- sum(log(eigen(Gamma)$values))
-		logdet.G <- -sum(log(eigen(Ginv)$values))	# We should be able to precompute most of this!!
+		logdet.Omega <- sum(log(Omega))
+
+		# Computing eigen on Gamma is very slow.
+		# It is much faster to use chol, then grab the diagonals
+		# See http://stackoverflow.com/questions/18117218/alternative-ways-to-calculate-the-determinant-of-a-matrix-in-r
+		# logdet.Gamma <- sum(log(eigen(Gamma)$values))
+		L <- chol(Gamma)
+		logdet.Gamma <- 2 * sum(log(diag(L)))
+
+		logdet.G <- n_mu*log(sig2mu) + r*log(sig2K) - Data$logdet.Cinv
 
 		logC <- 0.5 * logdet.Gamma + 0.5 * logdet.Omega - 0.5 * logdet.G -
-			0.5 * t(Data$y) %*% Omega %*% Data$y +
+			0.5 * t(Data$y) %*% (Omega * Data$y) +
 			0.5 * t(nu) %*% Gamma %*% nu
 
 		# TBD: Replace with our prior
-		lprior <- dgamma(sig2mu, 1, 1, log = TRUE) + dgamma(sig2K, 1, 1, log = TRUE) + dgamma(sig2xi, 1, 1, log = TRUE)
+		lprior <- dgamma(sig2mu, 1, 1, log = TRUE) +
+			dgamma(sig2K, 1, 1, log = TRUE) +
+			dgamma(sig2xi, 1, 1, log = TRUE)
 		ljacobian <- log(sig2mu) + log(sig2K) + log(sig2xi)
-		ret <- logC + lprior + ljacobian
+		ret <- as.numeric(logC + lprior + ljacobian)
 		if (is.infinite(ret)) browser()
 		ret
 	}
@@ -81,9 +98,12 @@ metrop.stcos <- function(Z, S, sig2eps, C.inv, H, R,
 	} 
 
 	browser()
-	rw.out <- rwmetrop(par.init = log(c(1,1,1)), logpost = logpost, R = R,
+	par.init <- log(c(init$sig2mu, init$sig2K, init$sig2xi))
+	logpost(par.init, Data)
+
+	rw.out <- rwmetrop(par.init = par.init, logpost = logpost, R = R,
 		burn = burn, thin = thin, report.period = report.period, Data = Data,
-		proposal = list(scale = 0.1, var = 1))
+		proposal = list(scale = 0.1, var = 1), use.cpp = FALSE)
 	print(rw.out$accept)
 	par.mcmc <- mcmc(rw.out$par)
 	phi.mcmc <- exp(par.mcmc)
