b93adfe62da6da81ef0ad666166457e26770fabd
Andrew Raim <andrew.raim@gmail.com>
2017-02-07 10:56:07 -0500

    Try to fit example with NUTS sampler from Laplace's Demon package

--- inst/note-20170202/src/driver.R
+++ inst/note-20170202/src/driver.R
@@ -1,7 +1,6 @@
-# Note: OverdispersionModelsInR is a package written by Andrew.
-# Recent versions with rwmetrop were never made public.
-library(OverdispersionModelsInR)
+library(stcos)
 library(coda)
+library(LaplacesDemon)
 
 # ----- A simple test case -----
 n <- 400
@@ -51,7 +50,8 @@ rBeta <- function(par, Data) {
 	return(Beta.draws)
 } 
 
-rw.out <- rwmetrop(par.init = log(0.25), logpost = logpost, R = 3000,
+par.init <- log(0.25)
+rw.out <- rwmetrop(par.init = par.init, logpost = logpost, R = 3000,
 	burn = 500, thin = 5, report.period = 100, Data = Data,
 	proposal = list(scale = 0.1, var = 1))
 print(rw.out$accept)
@@ -62,3 +62,48 @@ summary(phi.mcmc)
 
 Beta.mcmc <- mcmc(rBeta(par.mcmc, Data))
 plot(Beta.mcmc)
+
+# ----- Now let's try Laplace's Demon, which provides some fancier samplers -----
+
+model <- function(parm, Data) {
+	### Parameters
+	phi <- as.numeric(exp(parm))
+	G <- diag(1, 2)
+
+	### Log-Likelihood
+	Omega <- diag(1/phi^2, n)
+	Gamma <- t(Data$X) %*% Omega %*% Data$X + solve(G)
+	nu <- solve(Gamma, t(Data$X) %*% Omega %*% Data$y)
+
+	# logdet.Omega <- sum(log(eigen(Omega)$values))
+	logdet.Omega <- n*log(1 / phi^2)
+	logdet.Gamma <- sum(log(eigen(Gamma)$values))
+	logdet.G <- sum(log(eigen(G)$values))
+
+	logC <- 0.5 * logdet.Gamma + 0.5 * logdet.Omega - 0.5 * logdet.G -
+		0.5 * t(Data$y) %*% Omega %*% Data$y +
+		0.5 * t(nu) %*% Gamma %*% nu
+
+	### Log-Prior and Log-Jacobian
+	lprior <- dgamma(phi, 1, 1, log = TRUE)
+	ljacobian <- log(phi)
+
+	### Log-Posterior
+	lp <- logC + lprior + ljacobian
+
+	model.out <- list(LP=lp, Dev=-2*logC, Monitor=lp,
+		yhat = NULL, parm=parm)
+	return(model.out)
+}
+
+Data$parm.names <- c("par")
+Data$mon.names <- c("LP")
+ld.out <- LaplacesDemon(model, Data=Data, par.init, Covar=NULL,
+	Iterations=1000, Status=100, Thinning=1, Algorithm="NUTS",
+	Specs = list(A=500, delta=0.6, epsilon=NULL, Lmax=Inf))
+
+par.mcmc <- mcmc(ld.out$Posterior1)
+plot(par.mcmc)
+
+phi.mcmc <- exp(par.mcmc)
+plot(phi.mcmc)
