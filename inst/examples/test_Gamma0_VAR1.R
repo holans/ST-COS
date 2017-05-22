2896eaf57e3e179d6bd10cbe0588fc3a03a45a67
Andrew Raim <andrew.raim@gmail.com>
2017-05-22 06:34:25 -0400

    Added function to compute MC approx of lag 0 autocovariance

--- inst/examples/test_Gamma0_VAR1.R
+++ inst/examples/test_Gamma0_VAR1.R
@@ -1,12 +1,15 @@
 library(stcos)
+library(mvtnorm)
 
-m <- 500
+m <- 100
 
 A <- diag(0.5, m)
 diag(A[-m,-1]) <- 0.25
 diag(A[-1,-m]) <- 0.25
 
 Sigma <- diag(1, m)
+diag(Sigma[-m,-1]) <- 0.5
+diag(Sigma[-1,-m]) <- 0.5
 
 system.time(out <- solve_Gamma0(A, Sigma))
 
@@ -14,3 +17,34 @@ res <- solve(diag(nrow=m^2, x=1) - A %x% A, as.numeric(Sigma))
 
 cbind(out$x, res)
 sum(abs((out$x - res)))
+
+system.time(res.mc <- sample_Gamma0_VAR1(A, Sigma, R = 1000000, burn = 10000))
+cbind(out$x, as.numeric(res.mc))
+sum(abs((out$x - as.numeric(res.mc))))
+
+sample.Gamma0.VAR1 <- function(A, Sigma, R, burn = 10000)
+{
+	m <- nrow(Sigma)
+	Sigma.half <- chol(Sigma)
+	y.t.minus1 <- Sigma.half %*% rnorm(m)
+	Gamma0 <- matrix(0, m, m)
+	
+	for (t in 1:burn) {
+		z <- Sigma.half %*% rnorm(m)
+		y.t <- A %*% y.t.minus1 + z
+		y.t.minus1 <- y.t
+	}
+
+	for (t in 1:R) {
+		z <- Sigma.half %*% rnorm(m)
+		y.t <- A %*% y.t.minus1 + z
+		y.t.minus1 <- y.t
+		Gamma0 <- Gamma0 + (y.t %*% t(y.t)) / R
+	}
+
+	return(Gamma0)
+}
+
+system.time(res.mc <- sample.Gamma0.VAR1(A, Sigma, R = 10000000, burn = 10000))
+cbind(out$x, as.numeric(res.mc))
+sum(abs((out$x - as.numeric(res.mc))))
---
+++ src/sample_Gamma0_VAR1.cpp
@@ -0,0 +1,36 @@
+// [[Rcpp::depends("RcppArmadillo")]]
+#include <RcppArmadillo.h>
+
+/*
+ * TBD
+ * We need some Monte Carlo variance reduction for this to be usable with
+ * very large m. Otherwise, the approximation will not be very close even
+ * if R is taken to be very large.
+ */
+
+// [[Rcpp::export]]
+arma::mat sample_Gamma0_VAR1(const arma::mat& A, const arma::mat& Sigma, size_t R, size_t burn = 10000)
+{
+	size_t m = Sigma.n_rows;
+	arma::mat Sigma_half = arma::chol(Sigma);
+	arma::vec y_t_minus1 = Sigma_half * arma::randn(m);
+	arma::vec y_t;
+	arma::vec z;
+	arma::mat Gamma0(m, m);
+	Gamma0.fill(0);
+
+	for (size_t t = 0; t < burn; t++) {
+		z = Sigma_half * arma::randn(m);
+		y_t = A * y_t_minus1 + z;
+		y_t_minus1 = y_t;
+	}
+	
+	for (size_t t = 0; t < R; t++) {
+		z = Sigma_half * arma::randn(m);
+		y_t = A * y_t_minus1 + z;
+		y_t_minus1 = y_t;
+		Gamma0 += (y_t * trans(y_t)) / R;
+	}
+	
+	return Gamma0;
+}
