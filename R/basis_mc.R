c540a6432d42402f52b1acbfb6344611d61ff43f
Andrew Raim <andrew.raim@gmail.com>
2017-07-26 22:08:26 -0400

    I think it was a mistake to divide by |A|

--- R/basis_mc.R
+++ R/basis_mc.R
@@ -3,7 +3,6 @@ compute_sp_basis_mc <- function(basis, domain, R, report.period = 100)
 	n <- nrow(domain)
 	r <- basis$get_dim()
 	S <- Matrix(0, n, r)
-	areas <- as.numeric(st_area(domain))
 
 	for (j in 1:n) {
 		if (j %% report.period == 0) {
@@ -14,8 +13,6 @@ compute_sp_basis_mc <- function(basis, domain, R, report.period = 100)
 			P <- rArea(R, domain[j,])
 			S[j,] <- S[j,] + colSums(basis$compute(P[,1], P[,2]))
 		}
-
-		S[j,] <- S[j,] / areas[j]
 	}
 
 	return(S / R)
@@ -27,7 +24,6 @@ compute_spt_basis_mc <- function(basis, domain, R, period, report.period = 100)
 	r <- basis$get_dim()
 	S <- Matrix(0, n, r)
 	T <- length(period)
-	areas <- as.numeric(st_area(domain))
 
 	for (j in 1:n) {
 		if (j %% report.period == 0) {
@@ -38,8 +34,6 @@ compute_spt_basis_mc <- function(basis, domain, R, period, report.period = 100)
 			P <- rArea(R, domain[j,])
 			S[j,] <- S[j,] + colSums(basis$compute(P[,1], P[,2], period[t]))
 		}
-
-		S[j,] <- S[j,] / areas[j]
 	}
 
 	return( S / (R*T) )
