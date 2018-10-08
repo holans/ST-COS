0ddc0770aa15b4f4410181d4f4850632844174a8
Andrew Raim <andrew.raim@gmail.com>
2018-10-07 21:44:28 -0400

    Some cleanup

--- matlab/closelogger.m
+++
@@ -1,5 +0,0 @@
-function closelogger()
-	global log_fid
-	fclose(log_fid);
-end
-
--- matlab/mvnrnd.m
+++
@@ -1,6 +0,0 @@
-function x = mvnrnd(mu, Sigma)
-    [p,q] = size(Sigma);
-    xt = chol(Sigma) * randn([p 1]) + mu';
-    x = xt';
-end
-
