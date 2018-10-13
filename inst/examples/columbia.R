ad8072c73fa01d1220292508fec49d85e156e54b
Andrew Raim <andrew.raim@gmail.com>
2018-10-13 18:49:39 -0400

    Added documentation to columbia example code

--- inst/examples/columbia.R
+++ inst/examples/columbia.R
@@ -1,25 +1,54 @@
-# Analysis of City of Columbia data
+#' ---
+#' title: Analysis of City of Columbia data
+#' date: October 2018
+#' output: pdf_document
+#' ---
+
+#' # Overview
+#' In this example, we are given four neighborhoods in the City of Columbia in
+#' Boone County, Missouri. We would like to produce model-based estimates of
+#' median household income for these neighborhoods based on 5-year ACS estimates
+#' for block-groups in Boone County, Missouri from 2012, 2013, 2014, and 2015.
+#' Therefore, the four neighborhoods will be our target supports, and the 2012,
+#' 2013, 2014, 2015 year block-groups will be our source supports.
 set.seed(1234)
 
-# ----- Load Fine-level Support -----
-library(jsonlite)
-library(sf)
-library(tigris)
-library(dplyr)
+#' # Loading Fine-level Support
+suppressMessages(library(jsonlite))
+suppressMessages(library(sf))
+suppressMessages(library(tigris))
+suppressMessages(library(dplyr))
+suppressMessages(library(ggplot2))
 
-dom.fine <- block_groups(state = '29', county = '019', year = 2015) %>% 
+#' Load the fine-level support via shapefile using the `tigris` package.
+#' For this, we will use the 2015 block-groups in Boone County, MO.
+#' Convert it to an `sf` object, and transform to the projection with EPSG
+#' code 3857.
+options(tigris_use_cache = TRUE)
+options(tigris_refresh = FALSE)
+dom.fine <- suppressMessages(block_groups(state = '29', county = '019', year = 2015)) %>%
 	st_as_sf() %>%
 	st_transform(crs = 3857) %>%
 	mutate(geoid = GEOID) %>%
-	dplyr::select(-GEOID)
-plot(dom.fine[,1])
+	select(-GEOID)
 
-# ----- Assemble Source Supports -----
+#' A quick plot of the fine-level domain.
+ggplot(dom.fine) +
+	geom_sf(colour = "black", size = 0.05) +
+	ggtitle("Boone County, Missouri") +
+	theme_bw()
 
-# https://www.census.gov/data/developers/guidance/api-user-guide.html
-# https://api.census.gov/data/2015/acs5?get=NAME,B19013_001E&for=block%20group:*&in=state:29+county:019
-# https://api.census.gov/data/2015/acs5?get=NAME,B19013_001M&for=block%20group:*&in=state:29+county:019
-# Note: ACS 2016 data is not yet available via Census API
+#' # Assemble Source Supports
+#' The following loop pulls ACS direct estimates and associated MOEs from the
+#' Census Bureau's Data API, and merges them into a single data frame. For more
+#' information about the API, see
+#' <https://www.census.gov/data/developers/guidance/api-user-guide.html>.
+#' Examples of URLs to pull direct estimates and associated MOEs:
+#' 
+#' - <https://api.census.gov/data/2015/acs5?get=NAME,B19013_001E&for=block%20group:*&in=state:29+county:019>
+#' - <https://api.census.gov/data/2015/acs5?get=NAME,B19013_001M&for=block%20group:*&in=state:29+county:019>
+#' 
+#' Note: ACS 2016 data is not available via the API, at the time of this writing.
 
 year.levels <- 2012:2015
 dat.list <- list()
@@ -41,11 +70,11 @@ for (idx in 1:length(year.levels)) {
 	json_data <- fromJSON(moe_url)
 	moe_dat <- data.frame(json_data[-1,])
 	colnames(moe_dat) <- json_data[1,]
-	
+
 	my_dat <- est_dat %>%
 		inner_join(moe_dat, by = c('state' = 'state', 'county' = 'county',
 			'tract' = 'tract', 'block group' = 'block group')) %>%
-		dplyr::select(state, county, tract, blockgroup = `block group`,
+		select(state, county, tract, blockgroup = `block group`,
 			DirectEst = B19013_001E, DirectMOE = B19013_001M) %>%
 		mutate(state = as.character(state)) %>%
 		mutate(county = as.character(county)) %>%
@@ -55,14 +84,14 @@ for (idx in 1:length(year.levels)) {
 		mutate(DirectMOE = as.numeric(as.character(DirectMOE))) %>%
 		mutate(DirectVar = (DirectMOE / qnorm(0.95))^2)
 
-	my_shp <- block_groups(state = '29', county = '019', year = year) %>% 
+	my_shp <- block_groups(state = '29', county = '019', year = year) %>%
 		st_as_sf() %>%
 		st_transform(crs = 3857)
 
 	my_sf <- my_shp %>%
 		inner_join(my_dat, by = c('STATEFP' = 'state', 'COUNTYFP' = 'county',
 			'TRACTCE' = 'tract', 'BLKGRPCE' = 'blockgroup')) %>%
-		dplyr::select(geoid = GEOID, state = STATEFP, county = COUNTYFP,
+		select(geoid = GEOID, state = STATEFP, county = COUNTYFP,
 			tract = TRACTCE, blockgroup = BLKGRPCE,
 			DirectEst, DirectMOE, DirectVar)
 
@@ -73,38 +102,51 @@ for (idx in 1:length(year.levels)) {
 		filter(is.na(DirectEst))
 }
 
+#' Our assembled source supports are now acs5.2012, ..., acs5.2015
 acs5.2012 <- dat.list[[1]]
 acs5.2013 <- dat.list[[2]]
 acs5.2014 <- dat.list[[3]]
 acs5.2015 <- dat.list[[4]]
 rm(dat.list)
 
-plot(acs5.2015[,'DirectEst'])
+#' A quick plot of one of the source supports.
+ggplot(acs5.2015) +
+	geom_sf(colour = "black", size = 0.05, aes(fill = DirectEst)) +
+	scale_fill_distiller("DirectEst", palette = "RdYlBu") +
+	theme_bw()
 
-# ----- Load Target Supports -----
+#' # Load Target Supports
+#' Make sure to transform to the same projection as the fine-level support.
 neighbs <- st_read("shp/neighborhoods.shp") %>%
 	st_transform(crs = st_crs(dom.fine))
-plot(neighbs)
 
-# ----- Prepare to fit the model -----
-library(fields)
-library(stcos)
-library(ggplot2)
-library(ggforce)
+#' A quick plot of the neighborhoods.
+ggplot(neighbs) +
+	geom_sf(colour = "black", size = 0.05) +
+	theme_bw()
 
-# Spatial knots selected via space-filling design
+#' # Prepare to fit the model
+suppressMessages(library(fields))
+suppressMessages(library(stcos))
+suppressMessages(library(ggforce))
+
+#' Select spatial knots via space-filling design. Start with uniformly
+#' drawn points from the fine-level geography and use `cover.design`
+#' to select a subset of those points for knots.
 u <- st_sample(dom.fine, size = 2000)
 M <- matrix(unlist(u), length(u), 2, byrow = TRUE)
 out <- cover.design(M, 500)
 knots.sp <- out$design
 
-# Temporal knots are selected to be evenly spaced
+#' Select temporal knots to be evenly spaced over the years relevant
+#' to the source support years.
 knots.t <- seq(2008, 2015, by = 0.5)
 
-# Combined spatio-temporal knots
+#' Use Cartesian join of spatial and temporal knots to obtain spatio-temporal knots
 knots <- merge(knots.sp, knots.t)
 basis <- SpaceTimeBisquareBasis$new(knots[,1], knots[,2], knots[,3], w.s = 1, w.t = 1)
 
+#' Here is a plot of the spatial knots.
 knots.sp.dat <- data.frame(x = knots.sp[,1], y = knots.sp[,2], r = basis$get_rl())
 g <- ggplot(dom.fine) +
 	geom_sf(colour = "black", size = 0.25, fill = NA) +
@@ -116,20 +158,21 @@ g <- ggplot(dom.fine) +
 print(g)
 ggsave(g, filename = "spatial-knots.pdf", width = 4, height = 7)
 
-# Create an STCOSPrep object and add source supports
+#' # Build terms for STCOS model
+#' Create an STCOSPrep object and add source supports
 sp <- STCOSPrep$new(fine_domain = dom.fine, fine_domain_geo_name = "geoid", basis = basis, basis_mc_reps = 500)
 sp$add_obs(acs5.2012, period = 2008:2012, estimate_name = "DirectEst", variance_name = "DirectVar", geo_name = "geoid")
 sp$add_obs(acs5.2013, period = 2009:2013, estimate_name = "DirectEst", variance_name = "DirectVar", geo_name = "geoid")
 sp$add_obs(acs5.2014, period = 2010:2014, estimate_name = "DirectEst", variance_name = "DirectVar", geo_name = "geoid")
 sp$add_obs(acs5.2015, period = 2011:2015, estimate_name = "DirectEst", variance_name = "DirectVar", geo_name = "geoid")
 
-# Read the objects needed for MCMC
+#' Read the objects needed for MCMC
 z <- sp$get_z()
 v <- sp$get_v()
 H <- sp$get_H()
 S <- sp$get_S()
 
-# Some dimension reduction
+#' Dimension reduction of `S` matrix via PCA.
 eig <- eigen(t(S) %*% S)
 rho <- eig$values
 
@@ -139,6 +182,7 @@ f <- function(S) { S %*% Tx.S }
 sp$set_basis_reduction(f)
 S.reduced <- sp$get_reduced_S()
 
+#' Plot the proportion of variation captured by our selection of PCA components.
 pdf("pca-reduction.pdf", width = 7, height = 5)
 eigprops <- cumsum(rho) / sum(rho)
 plot(eigprops[1:200], xlab = "Dimension", ylab = "Proportion of Variation")
@@ -146,7 +190,7 @@ abline(v = max(idx.S), lty = 2)
 abline(h = eigprops[max(idx.S)], lty = 2)
 dev.off()
 
-# Pick a covariance structure for random coefficients of basis expansion
+#' Pick a covariance structure for random coefficients of basis expansion.
 if (FALSE) {
 	# Random Walk
 	K.inv <- sp$get_Kinv(2008:2015)
@@ -158,15 +202,14 @@ if (FALSE) {
 	K.inv <- diag(x = 1, nrow = ncol(S.reduced))
 }
 
-# Std'ize before MCMC
+#' Standardize observations before running MCMC.
 z.scaled <- (z - mean(z)) / sd(z)
 v.scaled <- v / var(z)
 
-# ----- Fit the Model -----
-library(coda)
+#' # Fit the Model
+suppressMessages(library(coda))
 
-# Use MLE as initial value for MCMC
-# This seems to fail sometimes when we don't have a lot of observations...
+#' Fit MLE; this will serve as an initial value for MCMC.
 K <- solve(K.inv)
 mle.out <- mle.stcos(z.scaled, v.scaled, H, S.reduced, K, init = list(sig2K = 1, sig2xi = 1))
 init <- list(
@@ -175,18 +218,16 @@ init <- list(
     mu_B = mle.out$mu.hat
 )
 
-# Gibbs Sampler
+#' Run the Gibbs sampler.
 gibbs.out <- gibbs.stcos.raw(z.scaled, v.scaled, H, S.reduced, K.inv, R = 10000,
 	report.period = 2000, burn = 2000, thin = 10, init = init)
 print(gibbs.out)
 
-pdf("trace.pdf")
+#' Show some trace plots to assess convergence of the sampler.
 plot((mu_B.mcmc <- mcmc(gibbs.out$mu_B.hist))[,1:3])
 plot((xi.mcmc <- mcmc(gibbs.out$xi.hist))[,1:3])
 plot((eta.mcmc <- mcmc(gibbs.out$eta.hist))[,1:3])
-dev.off()
 
-pdf("trace-varcomps.pdf", width = 7, height = 5)
 varcomps.mcmc <- mcmc(cbind(
 	gibbs.out$sig2mu.hist,
 	gibbs.out$sig2xi.hist,
@@ -194,11 +235,10 @@ varcomps.mcmc <- mcmc(cbind(
 ))
 colnames(varcomps.mcmc) <- c("sig2mu", "sig2xi", "sig2K")
 plot(varcomps.mcmc)
-dev.off()
 
-# ----- Produce Results on target supports -----
-# Compute H and S matrices and get summaries of posterior distribution for E(Y)
-# Use 90% significance for all credible intervals and MOEs
+#' #  Produce Results on target supports
+# Compute H and S matrices and get summaries of posterior distribution for E(Y).
+# Use 90% significance for all credible intervals and MOEs.
 
 targ.src <- sp$domain2model(acs5.2012, period = 2008:2012, geo_name = "geoid")
 E.hat.scaled <- fitted(gibbs.out, targ.src$H, targ.src$S.reduced)
@@ -251,12 +291,9 @@ neighbs$E.lo <- apply(E.hat, 2, quantile, prob = 0.05)
 neighbs$E.hi <- apply(E.hat, 2, quantile, prob = 0.95)
 print(neighbs)
 
-# Save progress here before attempting to make plots (that can always be done later)
-save.image("results.Rdata")
-
-# ----- Make some plots -----
-library(gridExtra)
-library(ggrepel)
+#' # Make some plots
+suppressMessages(library(gridExtra))
+suppressMessages(library(ggrepel))
 
 lim.est <- range(acs5.2015$DirectEst, acs5.2015$E.mean)
 g <- ggplot(acs5.2015) +
@@ -276,28 +313,28 @@ ggsave(h, filename = "compare2015-model.pdf", width = 5)
 
 g2012 <- ggplot(acs5.2012, aes(x=DirectEst, y=E.mean)) +
 	geom_point(size = 2) +
-	geom_abline(intercept = 0, slope = 1, color="red", 
+	geom_abline(intercept = 0, slope = 1, color="red",
 		linetype="dashed", size=1.2) +
 	ggtitle("2012 5yr ACS Direct Estimates") +
 	labs(x = "Direct Estimate", y = "Model-Based Estimate") +
 	theme_bw()
 g2013 <- ggplot(acs5.2013, aes(x=DirectEst, y=E.mean)) +
 	geom_point(size = 2) +
-	geom_abline(intercept = 0, slope = 1, color="red", 
+	geom_abline(intercept = 0, slope = 1, color="red",
 		linetype="dashed", size=1.2) +
 	ggtitle("2013 5yr ACS Direct Estimates") +
 	labs(x = "Direct Estimate", y = "Model-Based Estimate") +
 	theme_bw()
 g2014 <- ggplot(acs5.2014, aes(x=DirectEst, y=E.mean)) +
 	geom_point(size = 2) +
-	geom_abline(intercept = 0, slope = 1, color="red", 
+	geom_abline(intercept = 0, slope = 1, color="red",
 		linetype="dashed", size=1.2) +
 	ggtitle("2014 5yr ACS Direct Estimates") +
 	labs(x = "Direct Estimate", y = "Model-Based Estimate") +
 	theme_bw()
 g2015 <- ggplot(acs5.2015, aes(x=DirectEst, y=E.mean)) +
 	geom_point(size = 2) +
-	geom_abline(intercept = 0, slope = 1, color="red", 
+	geom_abline(intercept = 0, slope = 1, color="red",
 		linetype="dashed", size=1.2) +
 	ggtitle("2015 5yr ACS Direct Estimates") +
 	labs(x = "Direct Estimate", y = "Model-Based Estimate") +
@@ -311,7 +348,7 @@ acs5.2015$geoid_outlier <- ""
 acs5.2015$geoid_outlier[idx] <- acs5.2015$geoid[idx]
 g <- ggplot(acs5.2015, aes(x=DirectEst, y=E.mean)) +
 	geom_point(size = 2) +
-	geom_abline(intercept = 0, slope = 1, color="red", 
+	geom_abline(intercept = 0, slope = 1, color="red",
 		linetype="dashed", size=1.2) +
 	ggtitle("2015 5-year Estimates") +
 	labs(x = "Direct Estimate", y = "Model-Based Estimate") +
@@ -359,5 +396,3 @@ g <- ggplot(acs5.2015) +
 	theme_bw()
 print(g)
 ggsave(g, filename = "areas-of-interest-map.pdf")
-
-
